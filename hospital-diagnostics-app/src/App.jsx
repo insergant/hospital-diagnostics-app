b6fc5641cd99181edd4e4da8e72c30adf62584b7
Abdulrahman Hussein <abdulrahmanbibi@hotmail.com>
2026-05-31 16:21:20 +0300

    Fix: vercel code not found import error 2

--- hospital-diagnostics-app/src/App.jsx
+++ hospital-diagnostics-app/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import Register from "./pages/Register";
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+import Register from "./Pages/Register";
 
 function App() {
   return (
